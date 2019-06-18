586b6a7eae787fb7440fa6ecbaf44fc7c7f929d8
Tom Steventon <tomsteventon@gmail.com>
2019-06-18 08:47:13 +0100

    20190618d

--- data/boxfan/boxfan.lua
+++ data/boxfan/boxfan.lua
@@ -5,9 +5,11 @@ function onLoad(save_data)
 		inc          = 0.04,
 		running      = false,
 		repo_name    = "boxfan",
-		repo_version = "20190618c",
+		repo_version = "20190618d",
 		repo_url     = "https://raw.githubusercontent.com/stom66/tts-map-kit/master/data/",
 	}
+	checkForUpdates(fan.repo_name, fan.repo_version, fan.repo_url)
+
 	if save_data and save_data ~= "" then
 		local speed = tonumber(JSON.decode(save_data)[1])
 		if not speed then return false end
@@ -16,7 +18,12 @@ function onLoad(save_data)
 		Wait.frames(toggleFan, 1)
 		Wait.frames(|| setSpeed(speed), 2)
 	end
-	checkForUpdates(fan.repo_name, fan.repo_version, fan.repo_url)
+
+	--check we can access all the required components because for some reason we can get random errors if we dont check. weird.
+	if #self.getChildren() == 0 then self.reload() return end
+	if #self.getChildren()[1].getChildren() < 7 then self.reload() return end
+	if #self.getChildren()[1].getComponents() < 2 then self.reload() return end
+	if #self.getChildren()[1].getChildren()[7].getComponents() < 2 then self.reload() return end
 end
 function onSave()
 	if fan.running then
@@ -38,30 +45,15 @@ function toggleFan()
 end
 
 function setSpeed(val)
-	local function abort()
-		log("Fan is broken! Reloading")
-		self.reload()
-		return false
-	end
-
 	log("changeSpeed("..val..")")
-	fan.speed = tonumber(val)
+	fan.speed = tonumber(val)	
 	local actual_strength = fan.speed*fan.inc
-	local anim_speed = 0.5 + (fan.speed*0.15)
-
-	if not self.getChildren() then
-		return abort()
-	end
-	local windZone = self.getChildren()[1].getChildren()[7].getComponents()[2]
-	local animSpeed = self.getChildren()[1].getComponents()[2]
-
-	if not windZone or not animSpeed then
-		return abort()
-	end
-	
-	log("Setting fan speed to "..fan.speed.." ("..actual_strength.."), animation speed: "..anim_speed)	
+	local anim_speed      = 0.5 + (fan.speed*0.15)
+	local windZone        = self.getChildren()[1].getChildren()[7].getComponents()[2]
+	local animSpeed       = self.getChildren()[1].getComponents()[2]
 	windZone.set("windMain", actual_strength)
 	animSpeed.set("speed", anim_speed)
+	log("Setting fan speed to "..fan.speed.." ("..actual_strength.."), animation speed: "..anim_speed)	
 	updateBtnColors()
 end
 	function xml_setSpeed(player, val)
@@ -96,120 +88,5 @@ function updateBtnColors()
 	end
 end
 
-
---@@include updater.lua
-function checkForUpdates(asset, current_version, repo_url, timeout)
-
-	local updater = {
-		asset   = asset,
-		version = current_version,
-		repo    = repo_url,
-		timeout = timeout or 20,
-		cache = {
-			xml, lua, json
-		},
-		loading = {
-			xml = true,
-			lua = true,
-			json = true,
-		}
-	}
-	local url_s         = "?"..math.floor(os.time())
-	local url_base 		= updater.repo..updater.asset.."/"
-
-	updater.url = {
-		version = url_base.."version"..url_s,
-		lua     = url_base..updater.asset..".lua"..url_s,
-		xml     = url_base..updater.asset..".xml"..url_s,
-		json    = url_base..updater.asset..".json"..url_s
-	}
-
-	local function versionIsNewer(t_ver)
-		local c_ver = updater.version
-		local name = updater.asset
-		--check if target version [t_ver] is newer than the current [c_ver]
-		
-		--check for a straight match
-		if t_ver==c_ver then 
-			log("Asset "..name.." is up-to-date: "..t_ver)
-			return false
-		end
-
-		--check for newer date
-		local t_date = string.match(t_ver, "%d+")
-		local c_date = string.match(c_ver, "%d+")
-		if t_date > c_date then 
-			log("Asset "..name.." needs to be updated from "..c_ver.." to "..t_ver)
-			return true 
-		end
-
-		--check for same date but newer subversion
-		if t_date == c_date then
-			local t_rev = string.match(t_ver, "%a+") or 0
-			local c_rev = string.match(c_ver, "%a+") or 0
-			if string.byte(t_rev) > string.byte(c_rev) then 
-				log("Asset "..name.." needs to be patched from "..c_ver.." to "..t_ver)
-				return true 
-			end
-		end
-
-		--local copy is higher than remote copy
-		log("!#! Warning! asset "..name.." is a higher version than the repo")
-		return false
-	end
-	
-	--poll the repo and check the version file to see if we need to update
-	--if there's a version mismatch check if we're behind and fetch and apply
-	--the lua and xml before reloading the object	
-
-	WebRequest.get(updater.url.version, function(version_response)
-		if versionIsNewer(version_response.text) then
-			log("Updating "..updater.asset.." to version "..version_response.text)
-
-			for k,_ in pairs(updater.loading) do
-				log("   ...fetching new "..k.." from repo")
-				WebRequest.get(updater.url[k], function(r)
-					updater.cache[k] = r.text
-					updater.loading[k] = false
-				end)
-			end
-
-			--wait for both to complete before reloading
-			Wait.condition(
-				function() 
-					log("   ...new assets data loaded from repo")
-						local jsonSpawnObj     = JSON.decode(self.getJSON())
-						jsonSpawnObj.XmlUI     = updater.cache.xml
-						jsonSpawnObj.LuaScript = updater.cache.lua
-						--apply custom properties
-						for k,v in pairs(JSON.decode(updater.cache.json)) do
-							if jsonSpawnObj[k] then
-								for kk,vv in pairs(v) do
-									jsonSpawnObj[k][kk] = vv
-								end
-							end
-						end
-						--replace object
-						log("   ...destroying self and creating a replacement")
-						spawnObjectJSON({
-							json = JSON.encode(jsonSpawnObj)
-						})
-						self.destruct()
-					--
-				end,
-				function() 
-					return (
-						not updater.loading.lua and 
-						not updater.loading.xml and 
-						not updater.loading.json
-					) 
-				end,
-				timeout, --set above
-				function()
-					log("Unable to update asset "..asset..", update timed out after "..timeout.." seconds")
-				end
-			)
-		end
-	end)
-end
---@@end include updater.lua
+--updater
+function checkForUpdates(a,b,c,d)local e={asset=a,version=b,repo=c,timeout=d or 20,cache={xml,lua,json},loading={xml=true,lua=true,json=true}}local f="?"..math.floor(os.time())local g=e.repo..e.asset.."/"e.url={version=g.."version"..f,lua=g..e.asset..".lua"..f,xml=g..e.asset..".xml"..f,json=g..e.asset..".json"..f}local function h(i)local j=e.version;local k=e.asset;if i==j then log("Asset "..k.." is up-to-date: "..i)return false end;local l=string.match(i,"%d+")local m=string.match(j,"%d+")if l>m then log("Asset "..k.." needs to be updated from "..j.." to "..i)return true end;if l==m then local n=string.match(i,"%a+")or 0;local o=string.match(j,"%a+")or 0;if string.byte(n)>string.byte(o)then log("Asset "..k.." needs to be patched from "..j.." to "..i)return true end end;log("!#! Warning! asset "..k.." is a higher version than the repo")return false end;WebRequest.get(e.url.version,function(p)if h(p.text)then log("Updating "..e.asset.." to version "..p.text)for q,r in pairs(e.loading)do log("   ...fetching new "..q.." from repo")WebRequest.get(e.url[q],function(s)e.cache[q]=s.text;e.loading[q]=false end)end;Wait.condition(function()log("   ...new assets data loaded from repo")local t=JSON.decode(self.getJSON())t.XmlUI=e.cache.xml;t.LuaScript=e.cache.lua;for q,u in pairs(JSON.decode(e.cache.json))do if t[q]then for v,w in pairs(u)do t[q][v]=w end end end;log("   ...destroying self and creating a replacement")spawnObjectJSON({json=JSON.encode(t)})self.destruct()end,function()return not e.loading.lua and not e.loading.xml and not e.loading.json end,d,function()log("Unable to update asset "..a..", update timed out after "..d.." seconds")end)end end)end
